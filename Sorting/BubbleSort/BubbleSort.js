4b03f458850c68eed81d9b61767bece9853d3fc3
Anifah Bhadmus <abhadmus@appzonegroup.com>
2020-03-12 00:32:35 +0000

    completed method for bubble sort

---
+++ Sorting/BubbleSort/BubbleSort.js
@@ -0,0 +1,14 @@
+const bubbleSort = array => {
+let swaps=0;
+for(let i=0; i < array.length; i++){
+    if(array[i]> array[i+1]){
+        const temp = array[i];
+        array[i] = array[i+1];
+        array[i+1] = temp;
+        swaps++;
+    }
+}
+if(swaps===0) return array;
+else return bubbleSort(array);
+
+}
