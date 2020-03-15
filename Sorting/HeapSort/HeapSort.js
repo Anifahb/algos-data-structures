2578c694c0d5996c1b5d1f9a5739b126fb053f47
Anifah Bhadmus <abhadmus@appzonegroup.com>
2020-03-15 21:31:50 +0000

    completed method for merge sort

---
+++ Sorting/HeapSort/HeapSort.js
@@ -0,0 +1,20 @@
+
+//O(nlog(n) time| O(1) space0 average,worst and best case
+const heapSort = array => {
+
+
+   
+
+
+    return array;
+
+};
+
+
+const swap = (i, j, array) => {
+    let temp = array[j];
+    array[j] = array[i];
+    array[i] = temp;
+}
+
+module.exports = heapSort;
